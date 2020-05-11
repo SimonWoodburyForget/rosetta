4d3767e1abe53fc817d4b61851ab521327ee6030
simon-wf <simonwoodburyforget@gmail.com>
2020-05-11 01:17:31 -0400

    minor refactoring

--- list_comprehensions/src/lib.rs
+++ list_comprehensions/src/lib.rs
@@ -1,24 +1,32 @@
-/// Rust has powerful lazy iterator types:
-/// - `flat_map` maps range iterators and then flattens them;
-/// - `filter_map` maps the iterator and then filters it by `Some(value)`;
-/// - `impl Iterator` allows returning the iterator statically.
-pub fn pts(n: u32) -> impl Iterator<Item = (u32, u32, u32)> {
+/// Rust has powerful lazy iteration machinisms.
+///
+/// Primarily list-comprehension can be thought of as being
+/// made up of three parts:
+///
+/// 1. `flatten`
+/// 2. `filter`
+/// 3. `map`
+///
+/// We can compose iterators by nesting them into `flat_map`
+/// which is just a short-hand for `.map().flatten()`, and then
+/// we can use `Option` as an iterator, in `flap_map` which both
+/// builds the triplet `[x, y, z]` and then filters it.
+pub fn pts(n: u32) -> impl Iterator<Item = [u32; 3]> {
     (1..=n).flat_map(move |x| {
         (x..=n).flat_map(move |y| {
             (y..=n).filter_map(move |z| {
-                if x.pow(2) + y.pow(2) == z.pow(2) {
-                    Some((x, y, z))
-                } else {
-                    None
-                }
+                Some([x, y, z]).filter(|&[x, y, z]| x.pow(2) + y.pow(2) == z.pow(2))
             })
         })
     })
 }
 
+/// This macro makes simple use of the methods found
+/// on the `Iterator` trait and applies `flap_map`
+/// recursively.
 macro_rules! comp {
     ($e:expr; $x:pat in $xs:expr $(; $c:expr)?) => {{
-        $xs.filter_map(move |$x| if $($c &&)? true { Some($e) } else { None })
+        $xs.filter_map(move |$x| Some($e)$(.filter(|_| $c))?)
     }};
     ($e:expr; $x:pat in $xs:expr $(, $y:pat in $ys:expr)+ $(; $c:expr)?) => {{
         $xs.flat_map(move |$x| comp!($e; $($y in $ys),+ $(; $c)?))
@@ -26,11 +34,11 @@ macro_rules! comp {
 }
 
 /// Rust also has a powerful syntax extension system, with `macro_rules!`
-/// it's possible to build a macro which treats the input much like list
-/// comprehension, and just expands to iterator operations.
-pub fn pts1(n: u32) -> impl Iterator<Item = (u32, u32, u32)> {
+/// it's possible to build a macro which treats the input much like typical
+/// list comprehension, and just expand it to iterator operations.
+pub fn pts1(n: u32) -> impl Iterator<Item = [u32; 3]> {
     comp!(
-        (x, y, z);
+        [x, y, z];
         x in 1..=n,
         y in x..=n,
         z in y..=n;
@@ -47,12 +55,12 @@ mod tests {
 
         let v: Vec<_> = it.collect();
         let start = vec![
-            (3, 4, 5),
-            (5, 12, 13),
-            (6, 8, 10),
-            (8, 15, 17),
-            (9, 12, 15),
-            (12, 16, 20),
+            [3, 4, 5],
+            [5, 12, 13],
+            [6, 8, 10],
+            [8, 15, 17],
+            [9, 12, 15],
+            [12, 16, 20],
         ];
         let len = start.len();
         assert_eq!(&v[..len], &start[..]);
